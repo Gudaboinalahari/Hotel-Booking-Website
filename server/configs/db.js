c6b8235cd0e05e5d44ddc6d768267cd337f838a8
lahari <22eg105m43@anurag.edu.in>
2025-05-19 15:40:14 +0530

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async()=>{
+    try{
+
+    mongoose.connection.on('connected', ()=> console.log("Database connected"));
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/Hotel-Booking`)
+
+   } 
+   catch(error){
+    console.log(error.message);
+
+   }
+
+}
+
+
+
+export default connectDB;
